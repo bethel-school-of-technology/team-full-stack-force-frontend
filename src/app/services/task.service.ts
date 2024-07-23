4eef228ddd6e38afe41a94c05c75913b3b24e1cf
Jay Martin <jaywho111@hotmail.com>
2024-07-23 12:46:50 -0500

    added crud logic to task service

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Task } from '../models/task';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,23 +10,25 @@ export class TaskService {
 
   constructor(private http: HttpClient) { }
 
-  getAllTasks() {
+  baseUrl: string = "http://localhost:5025/api/task" //update to actual backend address
 
+  getAllTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.baseUrl)
   }
 
-  getTaskById(TaskId: number) {
-
+  getTaskById(taskId: number): Observable<Task> {
+    return this.http.get<Task>(`${this.baseUrl}/${taskId}`);
   }
 
-  createTask(newTask: Task) {
-
+  createTask(newTask: Task): Observable<Task> {
+    return this.http.post<Task>(this.baseUrl, newTask);
   }
 
-  updateTask(updatedTask: Task) {
-
+  updateTask(updatedTask: Task): Observable<Task>  {
+    return this.http.put<Task>(`${this.baseUrl}/${updatedTask.taskId}`, updatedTask);
   }
 
-  deleteTask(taskId: number) {
-
+  deleteTask(taskId: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${taskId}`);
   }
 }
