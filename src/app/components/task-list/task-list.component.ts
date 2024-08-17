3deb2583b8b08216e60dceddf312be81acc14cde
David Hay <yomanhay@comcast.net>
2024-08-17 18:24:01 -0400

    Added sorting for tasklist

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -8,11 +8,11 @@ import { Task } from '../../models/task';
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css'
 })
-export class TaskListComponent implements OnInit{
+export class TaskListComponent implements OnInit {
 
   taskList: Task[] = [];
 
-  constructor(private taskService: TaskService, private router: Router){}
+  constructor(private taskService: TaskService, private router: Router) { }
 
 
 
@@ -21,24 +21,33 @@ export class TaskListComponent implements OnInit{
   }
 
   loadTasks() {
-    this.taskService.getAllTasks().subscribe((task) => {
-      this.taskList = task.filter(t => !t.completed);
+    this.taskService.getAllTasks().subscribe((tasks) => {
+      const incompleteTasks = tasks.filter(t => !t.completed);
+
+      // Define the priority order
+      const priorityOrder = ['High', 'Medium', 'Low'];
+
+      // Sort tasks based on priority
+      this.taskList = incompleteTasks.sort((a, b) => {
+        if (a.priority === undefined) return 1;
+        if (b.priority === undefined) return -1;
+        return priorityOrder.indexOf(a.priority) - priorityOrder.indexOf(b.priority);
+      });
     }, error => {
       console.error('Error fetching tasks:', error);
     });
   }
-  
 
-  editTask(taskId: any){
+  editTask(taskId: any) {
     this.router.navigate(['/edit', taskId])
   }
 
-  deleteTask(taskId: any){
+  deleteTask(taskId: any) {
     this.taskService.deleteTask(taskId).subscribe(deleteTask => {
       console.log('Task deleted succesfully');
       this.loadTasks();
-    },error => {
-        console.error('Error deleting task:', error);
+    }, error => {
+      console.error('Error deleting task:', error);
     });
   }
 
